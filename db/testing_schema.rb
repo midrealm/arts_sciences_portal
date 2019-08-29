d53923460920b947d5850258b482557c63bfab11
Kelsey Shannahan <kelsey.shannahan@gmail.com>
2019-08-29 18:05:46 -0400

    adding user roles model + logic

--- db/testing_schema.rb
+++ db/testing_schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_08_27_122536) do
+
+  create_table "user_roles", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.string "role_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
