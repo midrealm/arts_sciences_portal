bec90900791e70d5db1b8c892b1dab3a82d15599
Kelsey Shannahan <kelsey.shannahan@gmail.com>
2019-12-11 09:07:10 -0500

    debugging in prod is the best

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -139,7 +139,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       :user_name => ENV['SENDGRID_USERNAME'],
       :password => ENV['SENDGRID_PASSWORD'],
-      :domain => 'a-and-s-portal.herokuapp.com',
+      :domain => 'https://a-and-s-portal.herokuapp.com',
       :address => 'smtp.sendgrid.net',
       :port => 587,
       :authentication => :plain,
