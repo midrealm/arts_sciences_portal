b32c31e5235683c494e45a88398178eb537ba165
Kelsey Shannahan <kelsey.shannahan@gmail.com>
2020-01-15 13:10:09 -0500

    proper deletion of entries

--- app/models/scoresheet.rb
+++ app/models/scoresheet.rb
@@ -1,7 +1,8 @@
 class Scoresheet < ApplicationRecord
   belongs_to :user
   belongs_to :entry
-  has_many :scores
+  has_many :scores, dependent: :destroy
+  
   accepts_nested_attributes_for :scores
 
   scope :for_user, -> (user) { where("user_id = ?", user.id)}
