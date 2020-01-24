fc4e189e41072edace3c78dc2b6214561529309d
Kelsey Shannahan <kelsey.shannahan@gmail.com>
2020-01-24 09:44:21 -0500

    restrict to admin

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,4 +1,6 @@
 class LocationsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :verify_admin
   before_action :set_location, only: [:show, :edit, :update, :destroy]
 
   # GET /locations
