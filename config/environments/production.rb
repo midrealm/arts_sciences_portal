af7022a999bbd4e11499a81e852d184bb2c0a5c7
fainting-goat <kelsey.shannahan@gmail.com>
2022-03-22 18:25:13 -0400

    use mailgun instead of twilio

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -133,16 +133,14 @@ Rails.application.configure do
   #   :authentication => :plain
   # }
 
-  # SEND GRID
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-      :user_name => ENV['SENDGRID_USERNAME'],
-      :password => ENV['SENDGRID_PASSWORD'],
-      :domain => 'herokuapp.com',
-      :address => 'smtp.sendgrid.net',
-      :port => 587,
-      :authentication => :plain,
-      :enable_starttls_auto => true
+  # MAILGUN
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'a-and-s-portal.herokuapp.com',
+    :authentication => :plain,
   }
+  ActionMailer::Base.delivery_method = :smtp
 end
