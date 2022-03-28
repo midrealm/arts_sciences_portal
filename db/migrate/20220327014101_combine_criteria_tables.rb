0244db95ea2a20ab873a817db7c438adccf8bbfa
fainting-goat <kelsey.shannahan@gmail.com>
2022-03-27 21:34:30 -0400

    add a default division for existing data

--- db/migrate/20220327014101_combine_criteria_tables.rb
+++ db/migrate/20220327014101_combine_criteria_tables.rb
@@ -2,7 +2,7 @@ class CombineCriteriaTables < ActiveRecord::Migration[6.0]
   def change
     rename_column :criteria_types, :description, :name
     add_column :criteria_types, :description, :text
-    add_reference :criteria_types, :division, null: false, foreign_key: true
+    add_reference :criteria_types, :division, null: false, foreign_key: true, default: 6
     remove_reference :criteria, :division, null: false, foreign_key: true
   end
 end
