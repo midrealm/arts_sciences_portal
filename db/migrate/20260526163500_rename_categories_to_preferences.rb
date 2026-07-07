e95d4f4e067ebdb6114c86994298a51508d5a7b3
fainting-goat <kelsey.shannahan@gmail.com>
2026-07-06 21:30:27 -0400

    migration fix

--- db/migrate/20260526163500_rename_categories_to_preferences.rb
+++ db/migrate/20260526163500_rename_categories_to_preferences.rb
@@ -1,10 +1,40 @@
 class RenameCategoriesToPreferences < ActiveRecord::Migration[6.0]
-  def change
-    rename_table :categories, :preferences
-    rename_column :judge_preferences, :category_id, :preference_id
-    rename_index :judge_preferences,
-                 :index_judge_preferences_on_category_id,
-                 :index_judge_preferences_on_preference_id
+  def up
+    if table_exists?(:categories) && !table_exists?(:preferences)
+      rename_table :categories, :preferences
+    elsif !table_exists?(:preferences)
+      create_table :preferences do |t|
+        t.string :name
+
+        t.timestamps
+      end
+    end
+
+    if column_exists?(:judge_preferences, :category_id)
+      rename_column :judge_preferences, :category_id, :preference_id
+    end
+
+    if index_name_exists?(:judge_preferences, "index_judge_preferences_on_category_id")
+      rename_index :judge_preferences,
+                   "index_judge_preferences_on_category_id",
+                   "index_judge_preferences_on_preference_id"
+    end
+  end
+
+  def down
+    if index_name_exists?(:judge_preferences, "index_judge_preferences_on_preference_id")
+      rename_index :judge_preferences,
+                   "index_judge_preferences_on_preference_id",
+                   "index_judge_preferences_on_category_id"
+    end
+
+    if column_exists?(:judge_preferences, :preference_id)
+      rename_column :judge_preferences, :preference_id, :category_id
+    end
+
+    if table_exists?(:preferences) && !table_exists?(:categories)
+      rename_table :preferences, :categories
+    end
   end
 end
 
--- db/migrate/20260526164500_create_entry_preferences.rb
+++ db/migrate/20260526164500_create_entry_preferences.rb
@@ -1,5 +1,7 @@
 class CreateEntryPreferences < ActiveRecord::Migration[6.0]
-  def change
+  def up
+    return if table_exists?(:entry_preferences)
+
     create_table :entry_preferences do |t|
       t.references :entry, null: false, foreign_key: true
       t.references :preference, null: false, foreign_key: true
@@ -8,6 +10,10 @@ class CreateEntryPreferences < ActiveRecord::Migration[6.0]
     end
 
     add_index :entry_preferences, [:entry_id, :preference_id], unique: true
+  end
+
+  def down
+    drop_table :entry_preferences if table_exists?(:entry_preferences)
   end
 end
 
