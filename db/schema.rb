f2ffc6deee69e0d29b30bd28125287f121a384b1
fainting-goat <kelsey.shannahan@gmail.com>
2026-07-06 21:22:00 -0400

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_26_164500) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_06_202400) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -26,6 +26,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_26_164500) do
   create_table "criteria", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.bigint "criteria_type_id", null: false
+    t.boolean "deprecated", default: false, null: false
     t.text "description"
     t.boolean "optional"
     t.integer "suggested_score"
@@ -60,6 +61,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_26_164500) do
 
   create_table "divisions", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.boolean "deprecated", default: false, null: false
     t.text "description"
     t.string "name"
     t.datetime "updated_at", null: false
